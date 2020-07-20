c07ba2ec160a1bdd1b88187487cd8d566f9ddf0d
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-07-19 21:31:03 -0300

    testing queries in the server

--- routes/api/firestore.js
+++ routes/api/firestore.js
@@ -1,15 +1,90 @@
 const express = require('express');
 const firebase = require('firebase');
+const moment = require('moment');
 
 const router = express.Router();
 
+const formatDoc = doc => {
+  const data = doc.data();
+
+  const formattedDoc = { id: doc.id, fields: {} };
+
+  for (const prop in data) {
+    let value = data[prop]
+    const type = Object.keys(doc._document.objectValue.proto.mapValue.fields[prop])[0];
+
+    if (type === 'timestampValue') value = moment(value.toDate()).format('lll');
+
+    formattedDoc.fields[prop] = { value, type };
+  }
+
+  return formattedDoc;
+}
+
 // @route   POST api/firestore/
-// @desc    test route
+// @desc    get data
 // @access  public
 router.post('/', (req, res) => {
-  console.log(req.body);
+  const { pathInputs, whereInputs, extraInputs } = req.body;
 
-  res.send('ok')
+  let query = firebase.firestore();
+
+  pathInputs.forEach(path => {
+    const { collectionName, documentId } = path;
+    if (collectionName) {
+      query = query.collection(collectionName.toString());
+      if (documentId) {
+        query = query.doc(documentId.toString());
+      }
+    }
+  });
+
+  whereInputs.forEach(where => {
+    let { fieldName, condition, fieldType, fieldValue } = where;
+    if (fieldName && condition && fieldType && fieldValue) {
+      fieldValue = fieldType === 'number' ? parseInt(fieldValue) : fieldValue;
+      query = query.where(fieldName.toString(), condition.toString(), fieldValue);
+    }
+  });
+
+  extraInputs.forEach(extra => {
+    const { extraType, extraValue } = extra;
+
+    if (extraType && extraValue) {
+      switch (extraType) {
+        case 'orderByAsc':
+          return query = query.orderBy(extraValue.toString(), 'asc');
+        case 'orderByDesc':
+          return query = query.orderBy(extraValue.toString(), 'desc');
+        case 'limit':
+          return query = query.limit(parseInt(extraValue));
+        case 'startAt':
+          return query = query.startAt(extraValue);
+        case 'startAfter':
+          return query = query.startAfter(extraValue);
+        case 'endBefore':
+          return query = query.endBefore(extraValue);
+        case 'endAt':
+          return query = query.endAt(extraValue);
+      }
+    }
+  });
+
+  query.get()
+    .then(snapshot => {
+      const result = [];
+      snapshot.forEach(doc => {
+        const { name, calories, fat, carbs, protein } = doc.data();
+        result.push({ _id: doc.id, name, calories, fat, carbs, protein });
+        // result.push(formatDoc(doc));
+      });
+
+      res.json(result)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Internal Server Error')
+    });
 });
 
 module.exports = router;
